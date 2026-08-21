105fb4f88f33fb0013171b15ee62d6a664ae8e2d
palletipad-byte <palletipad@gmail.com>
2026-08-21 19:48:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -584,7 +584,7 @@ elif choice.startswith("14."):
                     """
                     
                     response = client.models.generate_content(
-                        model='gemini-2.5-flash',
+                        model='gemini-3.6-flash',
                         contents=prompt
                     )
                     
