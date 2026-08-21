c91347ac771956b194519583cfcd5257f2ca5f37
palletipad-byte <palletipad@gmail.com>
2026-08-22 01:49:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ elif choice.startswith("2."):
                     """
                     
                     response = client.models.generate_content(
-                        model='gemini-3.6-flash',
+                        model='gemini-3.7-flash',
                         contents=[
                             prompt,
                             source_img,
